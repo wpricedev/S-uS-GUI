bbc96879459a1813ece166e80acd761b631c197d
Wade Price <wade00tert@gmail.com>
2018-04-10 16:10:42 +0100

    Update: Scraping is now functional!
    After a series of trial and error-esque implementations, a functional script has now been found to extract all necessary data.
    
    Refinement of this script and replication for other pages are now the goal, and after this commit, another should follow later today.
    
    As an afterthought:
    After the commit yesterday more research was gathered regarding 'how' scraping would occur, utilizing a 'headless' browser was the goal and found within selenium and an external driver. Currently there is a slight bit of concern with how this will pan out on release, as the developer is completely new to such things, but does realise the risk of the user needing the driver.
    
    A further commit should be posted later tonight with additional layouts for tree selections, leading up to tomorrows big 'connect the data' day.
    
    Although still behind, the project can still attain a reasonable finish date, it is believed.

--- ustream.py
+++ ustream.py
@@ -1,14 +1,38 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import bs4 as BeautifulSoup
 
 browse_url = "http://www.ustream.tv/explore/all"
-browser = webdriver.Chrome()
-browser.get(browse_url)
-html_source = browser.page_source
-browser.quit()
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--mute-audio')
+
+driver = webdriver.Chrome(chrome_options=options)
+
+driver.get(browse_url)
+html_source = driver.page_source
+driver.quit()
 
 soup = BeautifulSoup.BeautifulSoup(html_source, 'html.parser')
-viewers = soup.find("div", class_="media-data")
-
-for xd in viewers:
-    print(xd.get_text())
+#########################################################################################################
+# Get all broadcaster titles from the 'all' browse
+#########################################################################################################
+for h4 in soup.find_all('h4', class_="item-title"):
+    print(h4.find("a")['title'])
+#########################################################################################################
+# Get all broadcaster viewers from the 'all' browse
+#########################################################################################################
+for viewers in soup.find_all('span', class_="item-viewers"):
+    print(viewers.text)
+#########################################################################################################
+# Get all broadcaster url from the 'all' browse
+#########################################################################################################
+for url in soup.find_all('h4', class_="item-title"):
+    url_string = url.find("a")['href']
+    print("www.ustream.tv" + url_string)
+#########################################################################################################
+# Get all broadcaster thumbnails from the 'all' browse
+#########################################################################################################
+for thumbnail in soup.find_all('div', class_="item-image"):
+    print(thumbnail.find("img")['src'])
